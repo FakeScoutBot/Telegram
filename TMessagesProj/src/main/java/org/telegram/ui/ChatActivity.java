0871cad9f7a4caeffe0c50ac0911ff1b53d3085b
AndhaPaisa <gauravgoswami05668@gmail.com>
2026-07-27 17:36:43 +0530

    Fix

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -26348,6 +26348,14 @@ public class ChatActivity extends BaseFragment implements
                         }
                     }
                 }
+                boolean antiDeleteKeepInline = loadIndex == 0 && !obj.scheduled && AntiDeleteController.getInstance(currentAccount).shouldSaveDeletedMessage(dialog_id);
+                if (antiDeleteKeepInline) {
+                    obj.messageOwner.antiDeleted = true;
+                    if (editingMessageObject == obj) {
+                        hideFieldPanel(true);
+                    }
+                    updated = true;
+                } else {
                 obj.deleted = true;
                 if (obj.scheduled && sent) {
                     obj.scheduledSent = true;
@@ -26464,6 +26472,7 @@ public class ChatActivity extends BaseFragment implements
                         }
                     }
                     updated = true;
+                }
                 }
             }
         }
