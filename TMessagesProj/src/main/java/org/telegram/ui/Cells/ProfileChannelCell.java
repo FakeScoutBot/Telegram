569416d9ddf4e28f95bfb159a4a2096f0b86872a
AndhaPaisa <gauravgoswami05668@gmail.com>
2026-08-05 08:54:47 +0530

    Revert "Show full member/subscriber count instead of abbreviated (15.5K -> full number)"
    This reverts commit 16a3a7476aea5b2256278de8e11957c526e7df8e.

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ProfileChannelCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ProfileChannelCell.java
@@ -205,7 +205,10 @@ public class ProfileChannelCell extends FrameLayout implements Theme.Colorable {
         }
 
         if (channel != null) {
-            subscribersView.setText(LocaleController.formatPluralStringComma("Subscribers", channel.participants_count), true);
+            int[] result = new int[1];
+            boolean ignoreShort = AndroidUtilities.isAccessibilityScreenReaderEnabled();
+            String shortNumber = ignoreShort ? String.valueOf(result[0] = channel.participants_count) : LocaleController.formatShortNumber(channel.participants_count, result);
+            subscribersView.setText(LocaleController.formatPluralString("Subscribers", result[0]).replace(String.format("%d", result[0]), shortNumber), true);
 
             if (loading = (messageObjects == null || messageObjects.isEmpty())) {
                 dialogCell.setDialog(-channel.id, null, 0, false, animated);
